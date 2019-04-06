2750f0cbd1b4a3fd3ac14e0f2924703320b7381e
SuneClausen <sunekimclausen@hotmail.com>
2019-04-06 19:37:21 +0200

    save the pickupline to clip board

--- extension/content.js
+++ extension/content.js
@@ -5,6 +5,8 @@ document.addEventListener("mousedown", function(event){
     //right click
     if (event.button == 2) {
         clickedElement = event.target;
+        // findLowestChildTag();
+        alert(clickedElement.type); 
     }
 }, true);
 
@@ -16,23 +18,33 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
         xhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
                 clickedElement.value = xhttp.responseText; 
+                copyStringToClipboard(xhttp.responseText)
+                alert("saved to clip board"); 
             }
         };
-        xhttp.open("GET", "http://fapfap.e-software.dk/pickupline", true);
+        xhttp.open("GET", "https://fapfap.e-software.dk/pickupline", true);
         xhttp.send();
     }
 })
 
 // NOTE: Function to find child elements for further work in regards to FB-injection
-// function findLowestChildTag(){
-//     alert("FIND IT!"); 
-//     clickedElement.childNodes(); 
-//     while(clickedElement.childNodes.length > 0 ){
-//         clickedElement = clickedElement.childNodes[0];
-//     }
-//     clickedElement.parent();
-//     console.log("WOOOOOOOOOOORK!")
-//     console.log("clicked element" + clickedElement)
-//     alert(clickedElement.value); 
+function findLowestChildTag(){
+    document.getElementsByClassName(clickedElement.class); 
+}
 
-// }
+function copyStringToClipboard (str) {
+    // Create new element
+    var el = document.createElement('textarea');
+    // Set value (string to be copied)
+    el.value = str;
+    // Set non-editable to avoid focus and move outside of view
+    el.setAttribute('readonly', '');
+    el.style = {position: 'absolute', left: '-9999px'};
+    document.body.appendChild(el);
+    // Select text inside element
+    el.select();
+    // Copy text to clipboard
+    document.execCommand('copy');
+    // Remove temporary element
+    document.body.removeChild(el);
+ }
