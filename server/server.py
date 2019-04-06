c7ced95334fbb2b7b3dcb882ce595cf432941600
Johannes Ernstsen <ernstsen.johannes@gmail.com>
2019-04-06 13:29:40 +0200

    Added CORS allowance

--- server/server.py
+++ server/server.py
@@ -1,15 +1,19 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/pickupline')
 def get_pickup_line():
     return 'Dig. Mig. Spandauer.'
 
+
 @app.route('/')
 def root():
     return "The service for shy people in need of a hookup"
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
